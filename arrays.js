c95bf618870eb3cb7d2cf7cca25276549a7a6df4
Da Woon Lee <daniel.lee9415@gmail.com>
2017-11-15 21:39:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,40 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(arr, ele) {
-  var arr2 = [ele, ...arr];
-  return arr2;
+  arr = [ele, ...arr];
+  return arr;
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, ele) {
-  var arr2 = arr.unshift(ele);
-  return arr2;
-}
-
-function addElementToEndOfArray(arr, ele) {
-  var arr2 = [...arr, ele];
-  return arr2;
-}
-
-function destructivelyAddElementToEndOfArray(arr, ele) {
-  return arr.push(ele);
-}
-
-function accessElementInArray(arr, ind) {
-  return arr[ind];
-}
-
-function destructivelyRemoveElementFromBeginningOfArray(arr) {
-  return arr.shift();
-}
-
-function removeElementFromBeginningOfArray(arr) {
-  return arr.slice(1);
-}
-
-function destructivelyRemoveElementFromBeginningOfArray(arr) {
-  return arr.pop();
-}
-
-function removeElementFromEndOfArray(arr) {
-  return arr.slice(0, arr.length - 1);
+  arr.shift(ele);
+  return arr;
 }
